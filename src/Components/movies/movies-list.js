75227022d1fb54bd48c994ab75d224c3edaa659d
Jkobany <jkobany@gmail.com>
2021-01-30 17:00:23 +0200

    First UI for Homepage components

--- src/Components/movies/movies-list.js
+++ src/Components/movies/movies-list.js
@@ -1,10 +1,55 @@
+import $ from 'jquery'; 
+import React ,{useEffect} from 'react';
 import './movies.css'
 
 const MoviesListsComponent =()=>{
-        return (
-            <div className="movies-list">
-                <p>movies-list</p>
+        
+    
+    useEffect(()=>{
+        getMoviesByCategory('Comedy', '.list1');
+        getMoviesByCategory('Action', '.list2');
+        getMoviesByCategory('Drama', '.list3');
+        
+      })
+    
+    
+    async function getMoviesByCategory(category,list){
+        try{
+            let response = await fetch(`https://movies-smart.herokuapp.com/api/categories/${category}`);
+            if (response.ok) { 
+                let json = await response.json();
+                $(list).empty();
+                $(list).append(`<h4>${category}</h4>`);
+                json.forEach(movie => {
+                    $(list).append(
+                    '<article class="movie-mini hvr-curl-top-right hvr-shrink" onClick="sendIdToMovie(\'' + movie._id + '\')" >' +
+                    "<img src = '" +'https://movies-smart.herokuapp.com/' + movie.image + "'>" +
+                    "</article>"
+            );
+        });
+                console.log(json);
+            }
+        }
+        catch(error){
+            console.log(`error - getMoviesByCategory - ${error}`);
+        }
+    }
+    
+    
+    
+    
+    return (
+
+            <div id="pid">  
+            <div class="movies-list list1">
             </div>
+             <div class="movies-list list2">
+            </div>
+             <div class="movies-list list3">
+            </div>
+        </div>
+
+
         )
     }
 
--- src/Components/recomandations/recomandations-slider.js
+++ src/Components/recomandations/recomandations-slider.js
@@ -1,15 +1,75 @@
-import React, { Component}  from "react";
 import './recomandations.css'
 import background  from "../../img/background-1.jpg";
+import React,{useState,useEffect , useRef} from 'react';
+import $ from 'jquery'; 
 
-class RecomandationSliderComponent extends Component{
-    render(){
-        return (
-        <div id="our-recomandation">
-              <img src={background} alt="background"/>
-         </div>
-        )
+const RecomandationSliderComponent =()=>{
+    let index =0;
+    let [count, setCount] = useState(0);
+    const [data,setData]=useState([]);
+    async function getPopulars(){
+        try{
+            let response = await fetch('https://movies-smart.herokuapp.com/api/populars');
+            if (response.ok) { 
+                let json = await response.json();
+                setData(json);
+                displayPreviousImage(json , index);
+                index++;
+                if(index == 5) index = 0;
+                console.log(json);
+            }
+        }
+        catch(error){
+            console.log(`error - getPopulars - ${error}`);
+        }
     }
+    useEffect(()=>{
+        getPopulars()
+        
+      })
+
+       function useInterval(callback, delay) {
+    //     const savedCallback = useRef();
+      
+    //     // Remember the latest callback.
+    //     useEffect(() => {
+    //       savedCallback.current = callback;
+    //     }, [callback]);
+      
+    //     // Set up the interval.
+    //     useEffect(() => {
+    //       function tick() {
+    //         savedCallback.current();
+    //       }
+    //       if (delay !== null) {
+    //         let id = setInterval(tick, delay);
+    //         return () => clearInterval(id);
+    //       }
+    //     }, [delay]);
+       }
+
+
+
+
+      function displayPreviousImage(movies , index) {
+        $("#our-recomandation").empty();
+        let movie = movies[index];
+        $('#our-recomandation').append("<img src = '" +'https://movies-smart.herokuapp.com/' + movie.image + "' class='img-popular-big' >" );
+
+    }
+
+
+
+
+    
+     useInterval(() => {
+         setCount(count + 1);
+       }, 3000);
+    return (
+    <div id="our-recomandation">
+          <img src={background} alt="background"/>
+     </div>
+    )
 }
 
 export default RecomandationSliderComponent;
